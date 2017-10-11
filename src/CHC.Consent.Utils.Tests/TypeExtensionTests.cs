c494cfad3d17625d0ead84ae3972fe4073e5eadd
David Kemp <info@x2764te.ch>
2017-10-11 13:53:53 +0100

    Add type extension helpers

---
+++ src/CHC.Consent.Utils.Tests/TypeExtensionTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Xunit;
+
+namespace CHC.Consent.Utils.Tests
+{
+    public class TypeExtensionTests
+    {
+        private static readonly Type BaseType = typeof(Base);
+
+        public abstract class Base { } 
+        
+        public class SubClass : Base { }
+        
+        public class Unassociated { }
+
+        [Fact]
+        public void SubclassIsInheritedFromBase()
+        {
+            Assert.True(typeof(SubClass).IsInheritedFrom<Base>());
+        }
+
+        [Fact]
+        public void BaseIsInheritedFromBase()
+        {
+            Assert.True(BaseType.IsInheritedFrom<Base>());
+        }
+
+        [Fact]
+        public void UnassociatedClassIsNotInheritedFromBase()
+        {
+            Assert.False(typeof(Unassociated).IsInheritedFrom<Base>());
+        }
+
+        [Fact]
+        public void BaseIsNotSubclassOfBase()
+        {
+            Assert.False(BaseType.IsSubclassOf<Base>());
+        }
+
+        [Fact]
+        public void SubclassIsSubclassOfBase()
+        {
+            Assert.True(typeof(SubClass).IsSubclassOf<Base>());
+        }
+
+        [Fact]
+        public void UnassociatedIsNotSubclassOfBase()
+        {
+            Assert.False(typeof(Unassociated).IsSubclassOf<Base>());
+        }
+        
+    }
+}
---
+++ src/CHC.Consent.Utils/TypeExtension.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CHC.Consent.Utils
+{
+    public static class TypeExtension
+    {
+        public static bool IsInheritedFrom<T>(this Type @this) => @this.IsInheritedFrom(typeof(T));
+
+        public static bool IsInheritedFrom(this Type @this, Type baseType)
+        {
+            return baseType.IsAssignableFrom(@this);
+        }
+
+        public static bool IsSubclassOf<T>(this Type type) => type.IsSubclassOf(typeof(T));
+    }
+}
