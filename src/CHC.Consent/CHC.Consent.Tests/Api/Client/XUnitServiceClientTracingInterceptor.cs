fbfc1f8b1f98f061c4c85f53e9a8f69b44cf65ae
David Kemp <info@x2764te.ch>
2018-02-28 09:41:09 +0000

    Move class to it's own file

---
+++ src/CHC.Consent/CHC.Consent.Tests/Api/Client/XUnitServiceClientTracingInterceptor.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using Microsoft.Rest;
+using Xunit.Abstractions;
+
+namespace CHC.Consent.Tests.Api.Client
+{
+    internal class XUnitServiceClientTracingInterceptor : IServiceClientTracingInterceptor
+    {
+        public ITestOutputHelper Output { get; }
+
+        /// <inheritdoc />
+        public XUnitServiceClientTracingInterceptor(ITestOutputHelper output)
+        {
+            Output = output;
+        }
+
+        /// <inheritdoc />
+        public void Configuration(string source, string name, string value)
+        {
+            Output.WriteLine("Configuration: source:{0} name:{1} value:{2}", source, name, value);
+        }
+
+        /// <inheritdoc />
+        public void EnterMethod(string invocationId, object instance, string method, IDictionary<string, object> parameters)
+        {
+            
+        }
+
+        /// <inheritdoc />
+        public void ExitMethod(string invocationId, object returnValue)
+        {
+            
+        }
+
+        /// <inheritdoc />
+        public void Information(string message)
+        {
+            Output.WriteLine("Information: {0}", message);
+        }
+
+        /// <inheritdoc />
+        public void ReceiveResponse(string invocationId, HttpResponseMessage response)
+        {
+            Output.WriteLine("Response: InvocationId:{0} {1}",invocationId, response.AsFormattedString());
+        }
+
+        /// <inheritdoc />
+        public void SendRequest(string invocationId, HttpRequestMessage request)
+        {
+            
+            Output.WriteLine("Response: InvocationId:{0} {1}", invocationId, request.AsFormattedString());
+        }
+
+        /// <inheritdoc />
+        public void TraceError(string invocationId, Exception exception)
+        {
+            Output.WriteLine("Error: InvocationId:{0} {1}", invocationId, exception);
+        }
+    }
+}
