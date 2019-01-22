21e4f8de69852c70381ebaf0264933a0b3f41f5c
David Kemp <2870080+x2764tech@users.noreply.github.com>
2019-01-22 10:00:51 +0000

    Remove unused code from Xml Parser

--- src/CHC.Consent/CHC.Consent.DataImporter/XmlParser.cs
+++ src/CHC.Consent/CHC.Consent.DataImporter/XmlParser.cs
@@ -2,13 +2,11 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Reflection;
 using System.Text.RegularExpressions;
 using System.Xml;
 using System.Xml.Linq;
 using System.Xml.XPath;
 using CHC.Consent.Api.Client.Models;
-using CHC.Consent.Common.Infrastructure;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
 using Newtonsoft.Json;
@@ -184,149 +182,6 @@ namespace CHC.Consent.DataImporter
         }
 
 
-        private object ParseObject(XElement identifierNode, Dictionary<string, Type> typeNameLookup)
-        {
-            var typeName = identifierNode.Attribute("type")?.Value;
-            if (string.IsNullOrEmpty(typeName))
-            {
-                var nodeName = identifierNode.Name;
-                var prefix = string.IsNullOrEmpty(nodeName.NamespaceName) ? string.Empty : nodeName.NamespaceName + ".";
-                typeName = prefix + KebabCase(nodeName.LocalName);
-                //return null;
-            }
-
-            if (!typeNameLookup.ContainsKey(typeName))
-            {
-                throw new XmlParseException(identifierNode, $"Cannot find type '{typeName}'");
-            }
-
-            var identifierType = typeNameLookup[typeName];
-
-            return CreateObject(identifierType, identifierNode);
-        }
-
-        private object CreateObject(Type identifierType, XElement parentElement)
-        {
-            var constructor = GetConstructor(identifierType);
-
-
-            var parameters = constructor.GetParameters();
-            if (parameters.Length == 1)
-            {
-                var singleParameter = parameters[0];
-                if (CanHaveInlineValue(singleParameter.ParameterType))
-                {
-                    if (parentElement.IsEmpty || parentElement.FirstNode.NodeType == XmlNodeType.Text)
-                    {
-                        //TODO: throw error if it's the wrong number of parameters for the constructor 
-                        var node = parentElement.FirstNode;
-                        var stringValue = ((XText) node)?.Value;
-                        var type = singleParameter.ParameterType;
-
-                        var typedValue = TypedValue(parentElement, stringValue, singleParameter);
-                        Log.LogTrace(
-                            "Constructing {type} with {@typedValue} for {parameter}",
-                            type,
-                            typedValue,
-                            singleParameter);
-                        return constructor.Invoke(new[] {typedValue});
-                    }
-                }
-                else if(GetElementForParameter(parentElement, singleParameter) == null)
-                {
-                    return constructor.Invoke(new[] {CreateObject(singleParameter.ParameterType, parentElement)});
-                }
-            }
-
-            var parameterValues = new List<object>();
-            foreach (var parameter in parameters)
-            {
-                //TODO: add some checking here
-                var parameterElement = GetElementForParameter(parentElement, parameter);
-
-                if (parameterElement == null)
-                {
-                    if (parameter.HasDefaultValue)
-                    {
-                        parameterValues.Add(parameter.DefaultValue);
-                    }
-                    else
-                    {
-                        //TODO: Log this and handle more gracefully
-                        throw new NotImplementedException();
-                    }
-                }
-                else
-                {
-                    var value = (parameterElement.FirstNode as XText)?.Value;
-                    parameterValues.Add(TypedValue(parentElement, value, parameter));
-                }
-            }
-
-            Log.LogTrace("Constructing {type} with {@parametersValues}", constructor.ReflectedType, parameterValues);
-            return constructor.Invoke(parameterValues.ToArray());
-        }
-
-        private static XElement GetElementForParameter(XElement parentElement, ParameterInfo singleParameter)
-        {
-            return parentElement.Element(singleParameter.Name.ToKebabCase()) ?? parentElement.Element(singleParameter.Name);
-        }
-
-        private static ConstructorInfo GetConstructor(Type identifierType)
-        {
-            return identifierType.GetConstructors()
-                .Select(_ => new {Constructor = _, Parameters = _.GetParameters()})
-                .OrderByDescending(_ => _.Parameters.Length)
-                .Select(_ => _.Constructor)
-                .First();
-        }
-
-        private bool CanHaveInlineValue(Type type)
-        {
-            return
-                type.IsValueType ||
-                type.IsPrimitive ||
-                type == typeof(string);
-        }
-
-        private object TypedValue(XElement identifierNode, string stringValue, ParameterInfo parameterInfo)
-        {
-            object typedValue;
-            var parameterType = parameterInfo.ParameterType;
-            try
-            {
-                typedValue = ConvertStringToCorrectType(stringValue, parameterType);
-                
-            }
-            catch (FormatException e)
-            {
-                Log.LogError(
-                    e,
-                    "Error converting {value} to {type} for parameter {parameter}",
-                    stringValue,
-                    parameterType,
-                    parameterInfo.Name);
-                throw new XmlParseException(
-                    identifierNode,
-                    $"Error converting value to {parameterType} for {parameterInfo.Name}");
-            }
-
-            return typedValue;
-        }
-
-        private static object ConvertStringToCorrectType(string stringValue, Type type)
-        {
-            var underlyingType = Nullable.GetUnderlyingType(type);
-            if (underlyingType == null)
-            {
-                return ChangeType(stringValue, type);
-            }
-
-            if (string.IsNullOrEmpty(stringValue)) return null;
-
-            return ChangeType(stringValue, underlyingType);
-        }
-
         private static object ChangeType(string stringValue, Type type) => 
             type.IsEnum
             ? Enum.Parse(type, stringValue)
@@ -335,11 +190,5 @@ namespace CHC.Consent.DataImporter
         private static readonly Regex LowerCaseFollowedByUpperCase = new Regex(
             @"(\p{Ll})(\p{Lu})",
             RegexOptions.Compiled);
-
-
-        private static string KebabCase(string name)
-        {
-            return LowerCaseFollowedByUpperCase.Replace(name, match => match.Groups[1] + "-" + match.Groups[2].Value.ToLowerInvariant());
-        }
     }
 }
